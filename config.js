5cb6b964a5a053184c85946c06fda02d66b8d611
Kanyango <kariukikanyango@gmail.com>
2018-08-16 12:38:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.mongodb = {
     	//uri: 'mongodb://localhost:27017/kejadb'
-	uri: 'mongodb://karis:k3yb0@rd@ds121251.mlab.com:21251/kejadb'
+	uri: 'mongodb://kanyash:kanyash760@ds121251.mlab.com:21251/kejadb'
 	//uri: 'mongodb://kariuki:androidapps@ds033126.mlab.com:33126/smsappdb'
 };
 
